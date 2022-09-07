18499266b7b831659a24cc2d6c6c0f5cd3e691d3
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-09-07 15:49:53 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const yaml = require('js-yaml');
+
+export const parse = (markdown) => {
+  const separator = '---';
+  const lines = markdown.split('\n');
+  if (lines[0] !== separator || lines.filter(l => l === separator).length < 2) {
+    return { content: markdown, data: undefined };
+  }
+  
+  const endSeparatorIndex = lines.slice(1).indexOf(separator) + 1;
+  const frontMatter = lines.slice(1, endSeparatorIndex).join('\n');
+  let data = {};
+  try {
+    data = yaml.load(frontMatter);
+  } catch (err) {
+    console.log(err);
+  }
+
+  const content = lines.slice(endSeparatorIndex + 1).join('\n').trim();
+  return { content, data };
+};
