8548df4048210f3de78dd5becd5294882054dc07
fus1ondev <luke_s1220@yahoo.co.jp>
2023-04-03 12:07:19 +0900

    feat: 🎸 Rewrite with TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
-const yaml = require('js-yaml');
+import yaml from 'js-yaml';
 
-const matter = (markdown) => {
+const matter = (markdown: string) => {
   const separator = '---';
   const lines = markdown.split('\n');
+
+  // If the format is invalid, return the whole markdown as content
   if (lines[0] !== separator || lines.filter(l => l === separator).length < 2) {
     return { content: markdown, data: undefined };
   }
   
   const endSeparatorIndex = lines.slice(1).indexOf(separator) + 1;
   const frontMatter = lines.slice(1, endSeparatorIndex).join('\n');
-  let data = {};
+
+  // If the front matter is not valid YAML, return undefined as data
+  let data: any;
   try {
     data = yaml.load(frontMatter);
   } catch (err) {
     console.log(err);
+    data = undefined;
   }
 
   const content = lines.slice(endSeparatorIndex + 1).join('\n').trim();
   return { content, data };
 };
 
-module.exports = matter;
+export default matter;
